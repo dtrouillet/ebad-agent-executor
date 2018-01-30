b5de43136126bdc567f0bd79c474439490d14484
Damien Trouillet <Damien.Trouillet@caissedesdepots.fr>
2018-01-30 16:06:36 +0100

    Move in another project AgentInfos

--- src/main/java/fr/trouillet/faya/ebad/agent/Application.java
+++ src/main/java/fr/trouillet/faya/ebad/agent/Application.java
@@ -1,6 +1,5 @@
 package fr.trouillet.faya.ebad.agent;
 
-import fr.trouillet.faya.ebad.AgentInfos;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,13 +9,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJms
+@EnableScheduling
 public class Application {
 
     @Bean
@@ -45,19 +45,6 @@ public class Application {
     public static void main(String[] args) {
         // Launch the application
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-
-        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
-
-        // Send a message with a POJO - the template reuse the message converter
-        System.out.println("Sending an email message.");
-
-        AgentInfos agentInfos = new AgentInfos();
-        String[] app = {"er2", "eb1"};
-        agentInfos.setApplications(app);
-        agentInfos.setEnvironment("int");
-        agentInfos.setHost("dlj2eedeva51021");
-        agentInfos.setName("agent-1.0-SNAPSHOT");
-        jmsTemplate.convertAndSend("agent", agentInfos);
     }
 
 }
--- src/main/java/fr/trouillet/faya/ebad/agent/Sender.java
+++ src/main/java/fr/trouillet/faya/ebad/agent/Sender.java
@@ -1,7 +1,40 @@
 package fr.trouillet.faya.ebad.agent;
 
-/**
- * Created by dtrouillet on 29/01/2018.
- */
+
+import fr.trouillet.faya.ebad.parent.dto.AgentInfos;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessagePostProcessor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Component
 public class Sender {
+    private final JmsTemplate jmsTemplate;
+
+    public Sender(JmsTemplate jmsTemplate) {
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    public void healthCheck() throws UnknownHostException {
+        AgentInfos agentInfos = new AgentInfos();
+        String[] app = {"er2", "eb1"};
+        agentInfos.setApplications(app);
+        agentInfos.setEnvironment("int");
+        agentInfos.setHost(InetAddress.getLocalHost().getHostName());
+        agentInfos.setName("agent-1.0-SNAPSHOT");
+        jmsTemplate.convertAndSend("agent", agentInfos, new MessagePostProcessor() {
+            @Override
+            public Message postProcessMessage(Message message) throws JMSException {
+                message.setStringProperty("app","er2");
+                return message;
+            }
+        });
+    }
+
 }
